aa41412de8e00aa18529cba575f74e210545c612
Chase Robbins <chaserobbins@me.com>
2020-11-26 20:56:32 -0500

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Nav from './Nav.js';
+import Shop from './Shop.js';
+import About from './About.js';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+
+      </div>
+    </Router>
   );
 }
 
