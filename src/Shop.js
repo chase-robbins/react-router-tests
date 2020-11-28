9403a843cacfa8fbed804880dc4197ed67f542ab
Chase Robbins <chaserobbins@me.com>
2020-11-27 22:38:19 -0500

    final

--- src/Shop.js
+++ src/Shop.js
@@ -2,6 +2,9 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 
 function Shop() {
+  const fetchItems = async () => {
+    const data = await fetch('')
+  }
   return (
     <div>
       <h1> Shop Page </h1>
